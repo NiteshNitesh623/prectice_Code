b36744dc694cb09fff7e1095f05271dd2e2af86b
NiteshNitesh623 <niteshkanade228@gmail.com>
2024-10-13 13:41:52 +0000

    make some changes in Hello world

---
+++ c/Hello_world.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(int argc, char **argv){
+	printf("what is who are you ?\n");
+	printf("Hello world!\n");
+	printf("congrats, you've pushed your first code\n");
+	
+	return 0;
+}
