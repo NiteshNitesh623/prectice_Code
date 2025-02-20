4411a2ca668f93d8a7512bb16efdad3550f6d23d
nitesh kanade <84487062+NiteshNitesh623@users.noreply.github.com>
2025-02-20 08:29:08 +0530

    added some lines on this file

--- c/Hello_world.c
+++ c/Hello_world.c
@@ -1,3 +1,7 @@
 why i can't see any change here ?
 Now i can change my code directly from here 
 no need to fancy text editor.
+
+== idea ==
+    * i think about uploading all my vim wiki files here
+    * what if i write code directly here ?
